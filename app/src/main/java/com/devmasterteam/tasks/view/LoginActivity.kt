80376e6c20b1efdcfd380f3a22e0304d270258b1
Flaviota <flaviota.dev@gmail.com>
2024-01-16 14:00:53 -0300

    First commit

---
+++ app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
@@ -0,0 +1,38 @@
+package com.devmasterteam.tasks.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.devmasterteam.tasks.databinding.ActivityLoginBinding
+import com.devmasterteam.tasks.viewmodel.LoginViewModel
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Variáveis da classe
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        // Layout
+        setContentView(binding.root)
+
+        // Eventos
+        binding.buttonLogin.setOnClickListener(this)
+        binding.textRegister.setOnClickListener(this)
+
+        // Observadores
+        observe()
+    }
+
+    override fun onClick(v: View) {
+    }
+
+    private fun observe() {
+    }
+}
